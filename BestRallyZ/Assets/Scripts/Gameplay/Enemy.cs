1ec363de470fc1729ac534d0e68bf4e55bae16a0
PandeeDev <mayolchris@gmail.com>
2025-02-05 16:14:20 -0500

    New version

---
+++ BestRallyZ/Assets/Scripts/Gameplay/Enemy.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 3f;
+    private Transform Player;
+
+    void Start()
+    {
+        Player = GameObject.Find("Player").transform;
+    }
+
+    void Update()
+    {
+        // Move towards the player
+        Vector3 direction = (Player.position - transform.position).normalized;
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        PlayerController controller = other.GetComponent<PlayerController>();
+        if (controller != null)
+        {
+            controller.GameOver();
+        }
+    }
+}
